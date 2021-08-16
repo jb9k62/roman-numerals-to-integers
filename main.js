e84104785d4963ffc784f9c793d1dbb218e5ddca
jb9k62 <jb9k62@protonmail.com>
2021-08-16 11:33:51 +0200

    remove main comment

--- main.js
+++ main.js
@@ -8,47 +8,6 @@ numeralMap.set("C", 100);
 numeralMap.set("D", 500);
 numeralMap.set("M", 1000);
 
-/*
- * IV            = 4
- * VII           = 7
- * XXIV          = 24
- * XXXI          = 31
- * CCCLXIX       = 369
- * CD            = 400
- * CDXLVIII      = 448
- * MMDCCLI       = 2751
- *
- * smaller infront of larger = subtract
- * larger after smaller = addition
- * */
-
-/*
- * string
- * split -> array of chars
- * loop
- *   current
- *   previous
- *   next
- *   total
- *
- *  if first iter
- *   add to total
- *
- *  each iter thereafter,
- *   get value
- *     if next is truthy
- *          if next is equal or smaller
- *               ADD
- *          else
- *               take next value, subtraxct current, ?skip next iteration?
- *
- *     else
- *        if previous is equal or bigger
- *           ADD
- *        else
- *           SUBTRACT
- * */
-
 function numberalToInteger(numerals) {
   const chars = numerals.split("");
   let workingTotal = 0;
