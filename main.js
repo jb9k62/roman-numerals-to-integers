fe7b9d265df285679ecb795d510cef187be5def1
jb9k62 <jb9k62@protonmail.com>
2021-08-16 13:44:01 +0200

    add validation and tests

--- main.js
+++ main.js
@@ -8,20 +8,44 @@ numeralMap.set("C", 100);
 numeralMap.set("D", 500);
 numeralMap.set("M", 1000);
 
-function numberalToInteger(numerals) {
-  const chars = numerals.split("");
+function numeralToInteger(numerals = "") {
+  if (typeof numerals !== "string") {
+    throw new ValidationError("Incorrect input.");
+  }
+  const chars = numerals.toUpperCase().split("");
   let workingTotal = 0;
   let next;
 
   for (let index = 0; index < chars.length; index++) {
     const currentChar = chars[index];
+    if (new RegExp(`${currentChar}{4}`).test(numerals.substring(index))) {
+      throw new ValidationError(
+        "Four sequential occurrences of same numeral. Order should be different."
+      );
+    }
     const current = numeralMap.get(currentChar);
+    if (!current) {
+      throw new ValidationError("Unknown numeral encountered.");
+    }
     if (index === 0 && !chars[index + 1]) {
       workingTotal = workingTotal + current;
     } else {
       const nextChar = chars[index + 1];
       if (nextChar) {
         next = numeralMap.get(nextChar);
+        if (!next) {
+          throw new ValidationError("Unknown numeral encountered.");
+        }
+        const charAfterNext = chars[index + 2];
+        if (charAfterNext) {
+          const intAfterNext = numeralMap.get(charAfterNext);
+          if (!intAfterNext) {
+            throw new ValidationError("Unknown numeral encountered.");
+          }
+          if (next > current && intAfterNext > current) {
+            throw new ValidationError("Incorrect sequence of numerals.");
+          }
+        }
         if (next <= current) {
           workingTotal = workingTotal + current;
         } else {
@@ -42,6 +66,14 @@ function numberalToInteger(numerals) {
   return workingTotal;
 }
 
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
 module.exports = {
-  numberalToInteger,
+  numeralToInteger,
+  ValidationError,
 };
--- test.js
+++ test.js
@@ -1,14 +1,36 @@
 const assert = require("assert");
 
-const numberalToInteger = require("./main").numberalToInteger;
+const { numeralToInteger, ValidationError } = require("./main");
 
 console.log("Running tests...");
-assert.strictEqual(numberalToInteger("IV"), 4);
-assert.strictEqual(numberalToInteger("VII"), 7);
-assert.strictEqual(numberalToInteger("XXIV"), 24);
-assert.strictEqual(numberalToInteger("XXXI"), 31);
-assert.strictEqual(numberalToInteger("CCCLXIX"), 369);
-assert.strictEqual(numberalToInteger("CD"), 400);
-assert.strictEqual(numberalToInteger("CDXLVIII"), 448);
-assert.strictEqual(numberalToInteger("MMDCCLI"), 2751);
+assert.strictEqual(numeralToInteger("IV"), 4);
+assert.strictEqual(numeralToInteger("VII"), 7);
+assert.strictEqual(numeralToInteger("XXIV"), 24);
+assert.strictEqual(numeralToInteger("XXXI"), 31);
+assert.strictEqual(numeralToInteger("CCCLXIX"), 369);
+assert.strictEqual(numeralToInteger("CD"), 400);
+assert.strictEqual(numeralToInteger("CDXLVIII"), 448);
+assert.strictEqual(numeralToInteger("MMDCCLI"), 2751);
+assert.strictEqual(numeralToInteger("LIV"), 54);
+assert.throws(() => numeralToInteger([]), {
+  name: ValidationError.name,
+  message: "Incorrect input.",
+});
+assert.throws(() => numeralToInteger("IVL"), {
+  name: ValidationError.name,
+  message: "Incorrect sequence of numerals.",
+});
+assert.throws(() => numeralToInteger("XXXXI"), {
+  name: ValidationError.name,
+  message:
+    "Four sequential occurrences of same numeral. Order should be different.",
+});
+assert.throws(() => numeralToInteger("B"), {
+  name: ValidationError.name,
+  message: "Unknown numeral encountered.",
+});
+assert.throws(() => numeralToInteger("VB"), {
+  name: ValidationError.name,
+  message: "Unknown numeral encountered.",
+});
 console.log("Tests passed.");
